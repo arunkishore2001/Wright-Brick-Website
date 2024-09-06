52ce6cc6b635d205d8b207415b32b5f88ac8974c
Arun Kishore E <110062113+arunkishore2001@users.noreply.github.com>
2024-09-06 21:30:44 +0530

    added PHp

--- service.php
+++ service.php
@@ -1,3 +1,4 @@
+<?php include 'subheader.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -24,69 +25,7 @@
   </head>
 
   <body>
-    <header class="nav-bar-container">
-      <div class="container-fluid">
-        <div
-          class="navigation mt-3 d-flex justify-content-between align-items-center"
-        >
-          <div class="nav-main-wrapper">
-            <div class="nav-image d-md-block">
-              <img src="./img/logo.png" alt="Logo" />
-            </div>
-
-            <div
-              class="nav-link navigation-pages-wrapper d-md-flex align-items-center"
-            >
-              <nav class="d-md-flex">
-                <a  href="index.html">Home</a>
-                <a href="about.html">About Us</a>
-                <a class="active-nav" href="service.html">Services</a>
-                <a href="project.html">Projects</a>
-                <a href="contact.html">Gallery</a>
-              </nav>
-            </div>
-
-            <div class="contact-btn">
-              <a href="">Contact Us</a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="25"
-                viewBox="0 0 24 25"
-                fill="none"
-              >
-                <path
-                  d="M7 17.5L17 7.5M17 7.5H7M17 7.5V17.5"
-                  stroke="#D50F1C"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </div>
-
-            <!-- Burger Menu Button -->
-            <div class="burger-menu d-md-none" onclick="toggleMenu()">
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-
-          <div class="mobile-nav-wrapper d-md-none">
-            <div class="nav-menu-wrapper">
-              <div class="flex-column d-flex d-md-none align-items-center">
-                <a  href="index.html">Home</a>
-                <a class="nav-item" href="about.html">About Us</a>
-                <a  class="nav-item active-nav" href="service.html">Services</a>
-                <a class="nav-item" href="project.html">Projects</a>
-                <a class="nav-item" href="contact.html">Contact</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
+   
 
     <div class="container mt-5">
       <div class="entry-screen">
@@ -327,104 +266,7 @@
       </div>
     </main>
   
-    <footer>
-      <div class="container-fluid-max">
-        <div class="whole-footer">
-          <div class="footer-bg">
-            <div class="footer-img">
-              <img src="./img/footer-img.png" alt="" />
-              <div class="overlay-footer-text">
-                <h1>Have A Project In Mind?</h1>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-  
-      <div class="container-fluid-max position-relative">
-        <div class="footer-background-svg">
-          <svg
-            width="200"
-            height="210"
-            viewBox="0 0 293 323"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M6.53888 123.313C7.71042 120.965 10.8033 117.258 15.6455 112.445C20.4232 107.697 26.7211 102.048 34.0824 95.8429C48.801 83.4363 67.6867 68.8791 86.9718 54.9811C106.259 41.0819 125.911 27.8662 142.163 18.1343C150.293 13.2663 157.538 9.2897 163.444 6.53671C166.397 5.15996 168.985 4.10346 171.161 3.39459C173.366 2.67596 175.022 2.35742 176.152 2.35742C177.28 2.35742 178.91 2.6746 181.066 3.39096C183.193 4.09791 185.713 5.15219 188.581 6.52737C194.316 9.27732 201.318 13.2509 209.152 18.1173C224.813 27.8458 243.662 41.0584 262.093 54.9554C280.521 68.8513 298.496 83.405 312.41 95.8067C319.37 102.01 325.291 107.654 329.738 112.395C334.254 117.209 337.051 120.888 338.017 123.193C339.001 125.54 339.83 130.408 340.447 137.447C341.055 144.387 341.439 153.171 341.649 163.149C342.069 183.101 341.789 207.72 341.228 231.694C340.668 255.663 339.827 278.966 339.126 296.278C338.775 304.933 338.46 312.09 338.232 317.083C338.118 319.579 338.026 321.535 337.963 322.865C337.96 322.911 337.958 322.956 337.956 323.001H6.63742C6.63457 322.951 6.63166 322.899 6.62871 322.847C6.55314 321.517 6.44366 319.562 6.30809 317.066C6.03695 312.074 5.66149 304.917 5.24429 296.262C4.40984 278.952 3.40868 255.651 2.74131 231.685C2.07377 207.713 1.74096 183.1 2.24055 163.157C2.49042 153.183 2.94783 144.408 3.67049 137.48C4.40469 130.441 5.38756 125.62 6.53888 123.313Z"
-              stroke="#272727"
-              stroke-width="4"
-            />
-            <path
-              d="M122.501 189.284C122.675 189.089 123.167 188.73 124.216 188.304C125.218 187.897 126.549 187.499 128.197 187.121C131.489 186.367 135.852 185.738 140.963 185.24C151.172 184.245 164.164 183.791 177.106 183.844C190.048 183.897 202.881 184.458 212.774 185.48C217.728 185.991 221.889 186.613 224.939 187.33C226.469 187.689 227.658 188.058 228.505 188.421C229.304 188.764 229.573 189.008 229.62 189.051C229.628 189.058 229.63 189.059 229.627 189.055C229.627 189.055 229.627 189.055 229.628 189.056C229.634 189.068 229.714 189.213 229.827 189.591C229.939 189.964 230.054 190.466 230.169 191.108C230.397 192.39 230.599 194.104 230.772 196.213C231.117 200.425 231.337 206.06 231.456 212.65C231.696 225.822 231.537 242.683 231.216 259.326C230.896 275.964 230.415 292.363 230.015 304.604C229.814 310.724 229.634 315.804 229.504 319.354C229.449 320.855 229.403 322.083 229.368 323H122.9C122.847 322.087 122.778 320.858 122.694 319.352C122.499 315.825 122.229 310.776 121.929 304.691C121.328 292.52 120.607 276.206 120.127 259.628C119.646 243.043 119.407 226.223 119.766 213.03C119.946 206.427 120.275 200.778 120.789 196.536C121.047 194.412 121.346 192.691 121.681 191.4C122.039 190.018 122.367 189.432 122.501 189.284Z"
-              stroke="#272727"
-              stroke-width="4"
-            />
-          </svg>
-        </div>
-        <div class="end-footer-bg">
-          <div class="container">
-            <div class="end-top-footer">
-              <div class="end-left-intro">
-                <div class="end-text">
-                  <h4>WE WONDER TO CREATE INTERIORS</h4>
-                </div>
-                <div class="end-para mt-4">
-                  <p class="grey-text">
-                    Founded in 2007, Sparch is specializing in providing
-                    innovative services such as websites. Founded in 2007.
-                  </p>
-                </div>
-              </div>
-  
-              <div class="links d-flex">
-                <div class="company-link">
-                  <p>Company</p>
-                  <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Projects</li>
-                    <li>Portfolio</li>
-                  </ul>
-                </div>
-  
-                <div class="follow-us-link">
-                  <p>Follow Us</p>
-                  <ul>
-                    <li>Facebook</li>
-                    <li>Instagram</li>
-                    <li>LinkedIn</li>
-                    <li>YouTube</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-  
-          <div class="end-line">
-            <div class="container">
-              <div class="message">
-                <div class="call">
-                  <h6>Call our office</h6>
-                  <p class="grey-text">+91 999999999</p>
-                </div>
-  
-                <div class="send">
-                  <h6>Send a message</h6>
-                  <p class="grey-text">info@gmail.com</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-  
-        <div class="copyrights">
-          <p class="text-center">
-            Copyright 2024 © All Right Reserved @ High On Code
-          </p>
-        </div>
-      </div>
-    </footer>
+    <?php include 'footer.php'; ?>
 
     <script src="js/counting.js"></script>
     <script src="js/landing-slide.js"></script>
